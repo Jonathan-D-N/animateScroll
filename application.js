c28f5d66e146706f69290dd59cbce962a343c32f
Jonathan <jdneave@gmail.com>
2022-11-16 21:40:59 -0700

    tweak in view elements. Add out of view elements

--- application.js
+++ application.js
@@ -1,26 +1,48 @@
 const scrollElements = document.querySelectorAll(".js-scroll");
+
 //Set opacity of elements to 0
-scrollElements.forEach((el) => {
-    el.style.opacity = 0
-})
+//scrollElements.forEach((el) => {
+//    el.style.opacity = 0
+//})
 //Detect when an element is in view
-const elementInView = (el, scrollOffset = 0) => {
+const elementInView = (el, divident = 1) => {
     const elementTop = el.getBoundingClientRect().top;
 
     return (
         elementTop <= 
-        ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+        (window.innerHeight || document.documentElement.clientHeight) /
+    dividend
+        );
+};
+//
+
+const elementOutofView = (el) => {
+    elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop > (window.innerHeight ||
+            document.documentElement.clientHeight)
     );
 };
 
-Const displayScrollElement = (element) => {
+const displayScrollElement = (element) => {
     element.classList.add("scrolled");
+}
+
+const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
 }
 
 const handleScrollAnimation = () => {
     scrollElements.forEach((el) => {
         if (elementInView(el, 100)) {
             displayScrollElement(el);
+        } else {
+            hideScrollElement(el);
         }
     })
 }
+
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+})
